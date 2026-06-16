90b48b92f1b9f575ea24dfa8437fc226bb25518d
EmiyaNing <ningkangl@icloud.com>
2026-06-16 17:31:36 +0800

    a version for another visualize

--- visualize_color_utils.py
+++ visualize_color_utils.py
@@ -7,9 +7,8 @@ ADAPTIVE_BLUE = np.array([0.0, 0.25, 1.0], dtype=np.float32)
 
 
 def get_adaptive_gaussian_colors(pred, scales, opacities=None, seed=42):
-    """Choose Gaussian colors from opacity and raw mean scale."""
+    """Choose Gaussian colors by randomly splitting high-opacity Gaussians."""
     pred = np.asarray(pred, dtype=np.int64)
-    scales = np.asarray(scales, dtype=np.float32)
 
     if pred.size == 0:
         return np.empty((0, 3), dtype=np.float32)
@@ -18,11 +17,13 @@ def get_adaptive_gaussian_colors(pred, scales, opacities=None, seed=42):
         opacities = np.ones(len(pred), dtype=np.float32)
     opacities = np.asarray(opacities, dtype=np.float32).reshape(-1)
 
-    mean_size = scales.mean(axis=-1)
     out_colors = np.tile(ADAPTIVE_GRAY[None, :], (len(pred), 1))
 
-    high_opacity = opacities > 0.7
-    out_colors[high_opacity & (mean_size > 0.35)] = ADAPTIVE_RED
-    out_colors[high_opacity & (mean_size <= 0.35)] = ADAPTIVE_BLUE
+    high_opacity_indices = np.where(opacities > 0.7)[0]
+    rng = np.random.default_rng(seed)
+    rng.shuffle(high_opacity_indices)
+    red_count = len(high_opacity_indices) // 2
+    out_colors[high_opacity_indices[:red_count]] = ADAPTIVE_RED
+    out_colors[high_opacity_indices[red_count:]] = ADAPTIVE_BLUE
 
     return out_colors
